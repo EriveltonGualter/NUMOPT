f194b1fe313679c54437eaa4ce9f85a805903538
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-11-12 18:24:05 +0100

    Exercise 2 is done

--- exercise-04/main.m
+++ exercise-04/main.m
@@ -23,26 +23,26 @@ J = [x, ones(size(x))];
 v = (J'*J)\(J'*y);  % fitted coeffitiens [a, b] - no outliers
 vo = (J'*J)\(J'*yo);  % fitted coeffitiens [a, b] - outliers
 
-plot(x,v(1) * x + v(2), 'LineWidth', 1.2);  % plot fitted line: no outliers
-plot(x,vo(1) * x + vo(2), 'LineWidth', 1.2);  % plot fitted line: outliers
+plot(x,v(1)*x+v(2),'LineWidth',1.2);  % plot fitted line: no outliers
+plot(x,vo(1)*x+vo(2),'LineWidth',1.2);  % plot fitted line: outliers
+
+% 1c) Fit data with yalmip:
+vv = sdpvar(2,1);  % yalmip optimization variables
+f = (J * vv - y)' * (J * vv - y);  % yalmipt optimization function
+
+C = [];  % yalmip constraints
+opt = sdpsettings('solver','fmincon','verbose',2);
+diagn   = optimize(C, f, opt);
+V = double(vv);
+
+% plot yalmip fitting results:
+plot(x,V(1)*x+V(2),'--','LineWidth',1.2);  % plot fitted line: no outliers
 
 legend('Gaussian noise', 'Gasussian noise and outliers',...
        'Fitted line: no outliers',...
        'Fitted line: outliers',...
+       'Fitted line (yalmip): no outliers',...
        'Location', 'northwest');
-
-% 1c) Fit data with yalmip:
-% yalmip optimization variables
-yy = sdpvar(N,1);
-xx = sdpvar(N,1);
-vv = sdpvar(2,1);
-
-C = [xx == x, yy == y];  % yalmip constraints
-opt = sdpsettings('solver', 'fmincon','verbose',2,'usex0',0);
-diagn   = optimize(C, vv, opt);
-X = double(xx);
-Y = double(yy);
-V = double(vv);
 
 
 
