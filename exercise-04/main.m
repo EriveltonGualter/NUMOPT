21e7ae160afbaf27ee2cf3696a1ae2b3b73e36f3
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-11-13 12:08:39 +0100

    Exercise 3 is done

--- exercise-04/main.m
+++ exercise-04/main.m
@@ -28,20 +28,17 @@ plot(x,v2o(1)*x+v2o(2),'LineWidth',1.2);  % plot fitted line: outliers
 
 % 1c) Fit data with yalmip:
 vv = sdpvar(2,1);  % yalmip optimization variables
-f = (J * vv - y)' * (J * vv - y);  % yalmipt optimization function
+f2 = (J * vv - y)' * (J * vv - y);  % yalmipt optimization function
 
-C = [];  % yalmip constraints
-opt = sdpsettings('solver','fmincon','verbose',2);
-diagn   = optimize(C, f, opt);
+C2 = [];  % yalmip constraints
+opt2 = sdpsettings('solver','fmincon','verbose',2);
+diagn2 = optimize(C2, f2, opt2);
 v2y = double(vv);  % yalmip soution
-
-% plot yalmip fitting results:
-plot(x,v2y(1)*x+v2y(2),'--','LineWidth',1.2);  % plot fitted line: no outliers
 
 %% Exercise 3:
 
 % 3a) Write down needed vectors and matrices:
-f = [0; 0; ones(N,1)];
+f2 = [0; 0; ones(N,1)];
 b = [y; -y; zeros(N,1)];     % no outliers
 bo = [yo; -yo; zeros(N,1)];  % outliers
 
@@ -53,8 +50,9 @@ A = [ J,             -eye(N);...
      -J,             -eye(N);
      zeros(size(J)), -eye(N)];
 
-z = linprog(f,A,b,[],[],lb,ub);    % no outliers
-zo = linprog(f,A,bo,[],[],lb,ub);  % outliers
+% 3b) Solve the prblem:
+z = linprog(f2,A,b,[],[],lb,ub);    % no outliers
+zo = linprog(f2,A,bo,[],[],lb,ub);  % outliers
 
 v3 = z(1:2);
 v3o = zo(1:2);
@@ -63,10 +61,34 @@ v3o = zo(1:2);
 plot(x,v3(1)*x+v3(2),'-.','LineWidth',1.2);
 plot(x,v3o(1)*x+v3o(2),'-..','LineWidth',1.2);
 
+% 3c) Solve L1 fit problem with yalmip:
+s = sdpvar(N,1);
+v3y = sdpvar(2,1);
+v3oy = sdpvar(2,1);
+f3 = sum(s);
+
+% Yalmip settings:
+opt3 = sdpsettings('solver','fmincon','verbose',2);
+
+% Constraints without outliers:
+C3 = -s <= (v3y(1) * x + v3y(2) - y) <= s;
+C3 = [C3; -s <= 0];
+
+% Constraints with outliers:
+C3o = -s <= (v3oy(1) * x + v3oy(2) - yo) <= s;
+C3o = [C3o; -s <= 0];
+
+% Solve L1 problem without outliers:
+diagn3 = optimize(C3, f3, opt3);
+v3y = double(v3y);
+
+% Solve L1 problem with outliers:
+diagn3o = optimize(C3o, f3, opt3);
+v3oy = double(v3oy);
+
 legend('Gaussian noise', 'Gasussian noise and outliers',...
        'L2: no outliers',...
        'L2: outliers',...
-       'L2 (yalmip): no outliers',...
        'L1: no outliers',...
        'L1: outliers',...
        'Location', 'northwest');
