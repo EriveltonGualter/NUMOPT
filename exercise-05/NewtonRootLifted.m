5daaf2b1bdead29bb8a24c1cb8916cf8add9a08f
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-11-28 13:46:49 +0100

    Exercise 3 is in progress

--- exercise-05/NewtonRootLifted.m
+++ exercise-05/NewtonRootLifted.m
@@ -1,4 +1,4 @@
-function [z_log] = NewtonRootLifted(w0)
+function [z_log] = NewtonRootLifted(w0, n)
 % Implements newton root finding porblem for F(w) = w^16-2
 % s.t. F(w) = 0.
 %
@@ -6,6 +6,8 @@ function [z_log] = NewtonRootLifted(w0)
 %       w0 - initial guess as 4x1 vector
 %       type - gradient type: (use 'fixed' for fixed gradient
 %                              and something others for unfixed)
+
+N = n;  % maximal number of iterations
 
 % Function itself:
 F = @(w) [w(2) - w(1)^2;...
@@ -21,16 +23,21 @@ M = @(w) [-2*w(1), 0, 0, 0;...
 
 % Find minimum of function:
 wk = w0;       % initial point
-t = Inf;       % termination condition
-z_log = [];    % steps logger
-while t > 10^(-3)
-    z_log = [z_log, wk];
+z_log = zeros(max(size(w0)),N);  % allocate logger array
+f_log = [];
+for i=1:N
+    z_log(:,i) = wk;
+ 
     pk = M(wk)\F(wk);
     wk1 = wk - pk;
     wk = wk1;
-    t = norm(F(wk));
+
+    f_log = [f_log, norm(wk)];
+    if norm(wk) <= 10^(-3)
+        break;
+    end
 end
-z_log = z_log';
+z_log = z_log(:,1:i)';  % return only iteraded values
 end
 
 
