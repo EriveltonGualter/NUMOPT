71c1c1741759d9e70d3e89c9e6f7e538a3595caf
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-12-02 14:33:56 +0100

    Exercise 3c is done

--- exercise-05/NewtonRootLifted.m
+++ exercise-05/NewtonRootLifted.m
@@ -13,31 +13,29 @@ N = n;  % maximal number of iterations
 F = @(w) [w(2) - w(1)^2;...
           w(3) - w(2)^2;...
           w(4) - w(3)^2;...
-          -2 -   w(4)^2];
+          2    - w(4)^2];
 
 % Jacobian:
-M = @(w) [-2*w(1), 0, 0, 0;...
-          1, -2*w(2), 0, 0;...
-          0, 1, -2*w(3), 0;...
-          0, 0, 1, -2*w(4)];
+M = @(w) [-2*w(1), 1, 0, 0;...
+          0, -2*w(2), 1, 0;...
+          0, 0, -2*w(3), 1;...
+          0, 0, 0, -2*w(4)];
 
 % Find minimum of function:
 wk = w0;       % initial point
 z_log = zeros(max(size(w0)),N);  % allocate logger array
-f_log = [];
+
 for i=1:N
     z_log(:,i) = wk;
  
     pk = M(wk)\F(wk);
     wk1 = wk - pk;
-    wk = wk1;
-
-    f_log = [f_log, norm(wk)];
-    if norm(wk) <= 10^(-3)
+    if norm(wk1 - wk) <= 10^(-3)  % break if no changes in steps
         break;
     end
+    wk = wk1;
 end
-z_log = z_log(:,1:i)';  % return only iteraded values
+z_log = z_log(:,1:i)';  % return only iterated values
 end
 
 
