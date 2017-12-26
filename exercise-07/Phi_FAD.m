9c7fee89388d18410c8928e630c7398cd2e43795
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-12-26 18:43:48 +0100

    BFGS updated

--- exercise-07/Phi_FAD.m
+++ exercise-07/Phi_FAD.m
@@ -11,13 +11,13 @@ q  = param.q;
 X    = zeros(N,1);
 W    = zeros(N,1);  % dot quantities
 X(1) = x0;
-W(1) = 1;  % derivative over x0
+W(1) = 0;  % derivative over x0
 for k = 1:N-1
     X(k+1) = X(k) + h*((1 - X(k))*X(k) + U(k));
-    W(k+1) = 1 + h*(1-2*X(k));
+    W(k+1) = h*U(k);
 end
 
-J = q*2*W(1:end);
+J = W;
 
 % Add derivative of the quadratic term:
 J = 2*U + J;
