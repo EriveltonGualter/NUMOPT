b8439296514fbb880a3f62a6c0d322bc1bc26caa
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2018-01-22 11:14:07 +0100

    2. is done

---
+++ exercise-09/fminconSolution.m
@@ -0,0 +1,34 @@
+
+% a simple example using fmincon
+clear variables; close all; clc
+
+param.R = 8; % radius of constraint
+x0 = [1;1];  % initial condition for solver
+
+% Objective f = x_2:
+obj = @(x)(x(2)^4 + (x(1) + 2)^4);
+
+% Nonlinear constraints:
+% (we need to create a function that takes only the optimization variables
+% as input and keeps the parameters in param fixed)
+con = @(x)(fmincon_constraints(x, param));
+
+% Load default options for fmincon:
+opts = optimoptions('fmincon');
+
+% Example on how to change an option:
+opts.MaxFunEvals = 5000;
+
+% Call fmincon:
+[x_opt,~,~,~,lambdas] = fmincon(obj,x0,[],[],[],[],[],[],con,opts);
+
+% Dual multiplier of nonlinear equality constraint: lambdas.eqnonlin:
+
+% Plot result:
+plot_result(x_opt,param.R,...
+            'A simple example on equality constrained optimization');
+
+% check constraint violation at solution:
+[C, Ceq] = con(x_opt);
+disp('Constraint violation at solution:');
+disp(abs(Ceq));
---
+++ exercise-09/plot_result.m
@@ -0,0 +1,27 @@
+function [fig] = plot_result(x_opt, R, titleName)
+% Plots result for fmincon example
+
+ang = 0:0.01:2*pi; 
+xp  = R*cos(ang);
+yp  = R*sin(ang);
+
+n = 10;
+x1eq = linspace(-R,R,n)';
+x2eq = x1eq;
+
+fig = figure('Name',titleName);
+plot(xp, yp);
+hold('on');
+plot(x1eq,x2eq);
+hold('on');
+plot(x_opt(1),x_opt(2),'rx','MarkerSize',10);
+axis(R*[-1.1 1.1 -1.1 1.1]);
+title(titleName);
+legend('Inequality constraint',...
+       'Equality constraint',...
+       ['Optimal solution: [',num2str(x_opt(1)),...
+                            '; ',num2str(x_opt(2)),']'],...
+       'Location','best');
+
+end
+
